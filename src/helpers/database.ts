66056a3aaaed67c079581d30ed37b06c9b7476e5
Mostafa Safwat <36309847+010101mostafa@users.noreply.github.com>
2022-04-16 18:15:59 +0200

    ..

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -6,7 +6,7 @@ const {NODE_ENV, DB_NAME, DB_HOST, DB_USER, DB_PASSWORD,TEST_DB_NAME } = process
 let database:string|undefined="";
 if(NODE_ENV=='dev')
   database=DB_NAME;
-else if (NODE_ENV=='dev')
+else if (NODE_ENV=='test')
   database=TEST_DB_NAME; 
 const pool = new Pool({
   host: DB_HOST,
